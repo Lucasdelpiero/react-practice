e92d5d649cc3b0b6ff99a761ec9834309788c446
Lucas Del Piero <lucasdelpiero98@gmail.com>
2021-11-23 19:23:33 -0300

    second commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+//STORE -> GLOBALIZED STATE
+
+//ACTION INCREMENT
+
+//REDUCER
+
+//DISPATCH
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
